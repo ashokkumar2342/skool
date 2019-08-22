8bdbd4ca816c94a06490c385924bd2a32dab0a6f
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-08-22 13:10:45 +0530

    Update pdf_generate.blade.php

--- resources/views/admin/report/finalReport/pdf_generate.blade.php
+++ resources/views/admin/report/finalReport/pdf_generate.blade.php
@@ -248,6 +248,80 @@
   </div>
   @endif 
   @endforeach
+  @if (in_array(3,$sectionWiseDetails))
+  <h4 align="center"><b>medicalInfo Details</b></h4><hr> 
+   
+                        
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+     <p><li>On Date : ................ <b></b></li></p>
+   </div> 
+   <div class="col-lg-6">
+    <p><li>Blood Group : ................ </li></p>
+  </div>
+  </div>
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+    <p><li>HB : ................</li></p>  
+  </div> 
+  <div class="col-lg-6">
+    <p><li>BP : ................</li></p>
+  </div>
+  </div>
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+    <p><li>Height : ................</li></p>  
+  </div> 
+  <div class="col-lg-6">
+    <p><li>Weight : ................</li></p>
+  </div>
+  </div> 
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+     
+
+    <p><li>Physical Handicapped : ................</li></p>  
+    
+      
+    
+  </div> 
+  <div class="col-lg-6">
+    <p><li>Narration : ................</li></p>
+  </div>
+  </div> 
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+    
+
+    <p><li>Alergey : ................</li></p>  
+    
+  </div> 
+  <div class="col-lg-6">
+   <p><li>Alergey Vacc : ................</li></p>
+  </div>
+  </div> 
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+     <p><li>ID Mark 1 : ................</li></p>  
+   </div> 
+   <div class="col-lg-6">
+     <p><li>ID Marks : ................</li></p>
+   </div>
+  </div>
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6"> 
+     <p><li>Dental : ................</li></p>  
+   </div> 
+   <div class="col-lg-6">
+    <p><li>Vision : ................</li></p>
+  </div>
+  </div> 
+  <div class="row" style="margin-left: 80px"> 
+   <div class="col-lg-6">
+     <p><li>Complextion : ................</li></p>
+   </div>
+  </div>
+  @endif
   @foreach(App\Model\StudentSiblingInfo::where('student_id',$student->id)->get() as $studentSiblingInfo) 
   @if (in_array(4,$sectionWiseDetails))
 
