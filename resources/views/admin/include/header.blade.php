00fcbcfeb7c22cce88195409e4fb8ea10d405e91
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-09-17 21:09:10 +0530

    Update header.blade.php

--- resources/views/admin/include/header.blade.php
+++ resources/views/admin/include/header.blade.php
@@ -20,7 +20,7 @@
             <li class="dropdown messages-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-envelope-o"></i>
-              <span class="label label-success">4</span>
+              <span class="label label-success">1</span>
             </a>
             <ul class="dropdown-menu">
               <li class="header">You have 4 messages</li>
