a9fa156b3421ed94cf837b4807d7d2d03ace9c61
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-10-12 15:41:26 +0530

    Update table_show.blade.php

--- resources/views/schoolDetails/table_show.blade.php
+++ resources/views/schoolDetails/table_show.blade.php
@@ -1,15 +1,21 @@
 <table class="table"> 
 	<thead>
 		<tr>
-			<th>header</th>
+			<th>Name</th>
+			<th>Mobile No</th>
+			<th>Contact No</th>
+			<th>Logo</th>
 		</tr>
 	</thead>
 	<tbody>
 		@foreach ($SchoolDetails as $SchoolDetail)
 			 
-		@endforeach
 		<tr>
-			<td>data</td>
+			<td>{{ $SchoolDetail->name }}</td>
+			<td>{{ $SchoolDetail->mobile }}</td>
+			<td>{{ $SchoolDetail->contact }}</td>
+			<td>{{ $SchoolDetail->logo }}</td>
 		</tr>
+		@endforeach
 	</tbody>
 </table>
