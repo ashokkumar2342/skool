2c44e3eec3048bae42a14362dbe617e70f6ff3ad
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-11-15 16:14:20 +0530

    Update LoginController.php

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -75,7 +75,7 @@ class LoginController extends Controller
                    
             } 
 
-            $student = Student::orWhere('email',$request->email)->orWhere('username',$request->email)->first();
+            $student = Student::orWhere('username',$request->email)->first();
              if (!empty($student)) {
                  if (Hash::check($request->password, $student->password)) {
                      auth()->guard('student')->loginUsingId($student->id);
