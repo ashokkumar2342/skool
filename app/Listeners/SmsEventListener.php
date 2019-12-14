4facee40b9ccd932aefdc817bf99d77fbe23cc30
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-12-14 14:37:39 +0530

    Update SmsEventListener.php

--- app/Listeners/SmsEventListener.php
+++ app/Listeners/SmsEventListener.php
@@ -28,11 +28,9 @@ class SmsEventListener
      * @return void
      */
   public function handle(SmsEvent $event)
-  {  
+  {   Log::info('message sent');
         $smsApi=SmsApi::where('status',1)->first(); 
-        $admin=Admin::where('role_id',1)->first(); 
-
-        if ($smsApi->enableAutoSend==1) {
+        $admin=Admin::where('role_id',1)->first();        
          $msg=urlencode($event->message);
          
          $url = "$smsApi->url?user=$smsApi->user_id&pwd=$smsApi->password&senderid=$smsApi->sender_id&mobileno=$admin->mobile&msgtext=$msg";
@@ -40,11 +38,6 @@ class SmsEventListener
          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
          $curl_scraped_page = curl_exec($ch);
          curl_close($ch); 
-          Log::info($curl_scraped_page);
-        }
-        
-        
-    
-      
+          Log::info('message sent'); 
   }
 }
