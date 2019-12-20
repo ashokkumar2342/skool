e06b1cb4f661456e557c0a16c44ef3ae277b80a4
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-12-21 00:03:28 +0530

    fee details show

--- resources/views/admin/finance/feecollection/fee_detail_show.blade.php
+++ resources/views/admin/finance/feecollection/fee_detail_show.blade.php
@@ -2,7 +2,7 @@
 @php
 	$paid=0;
 	$concession_amount=0;
-	$net_amount=0;
+	$net_amount =0;
 @endphp
 <form  method="post" id="fee_collection_submit_form" accept-charset="utf-8">
 
@@ -20,96 +20,32 @@
 		  		</thead>
 		  		<tbody>
 		  			 <input type="checkbox" checked name="student_id[]" value="{{ $student->id }}" style="display: none">  
-		  			 <input type="hidden"  name="toDate" value="{{ $toDate }}">  
+		  			   
 		  			  
-		  			@foreach ($FeeDetails as $feeName =>$FeeDetail) 
-		  			@php
-		  				$concession_amount+=$FeeDetail->sum('concession_amount');
-		  				$net_amount+=$FeeDetail->sum('fee_amount')-$FeeDetail->sum('concession_amount');
-		  			@endphp
+		  			@foreach ($FeeDetails as $FeeDetail) 
+		  			 
 		  				<tr>  
-		  				  <td style="width: 250px">
-		  						 	{{ $feeName}}  
+		  				    <td style="width: 250px">
+		  					{{ $FeeDetail->name}}  
 		  					 </td>  
-					     			<td> 
-					 			    {{ $FeeDetail->sum('fee_amount')}}  
-					 		     </td> 
+					     	<td> 
+					 		{{ $FeeDetail->fee_amt}}   
+					 		</td> 
 		  				</tr> 
-		  				 
+		  				 @php
+		  				 	$net_amount += $FeeDetail->fee_amt;
+		  				 	if($FeeDetail->name=='Concession'){
+		  				 		$concession_amount=$FeeDetail->fee_amt;
+		  				 	}
+		  				 @endphp
 		  			@endforeach
-		  			@php
-		  				$net_amount+=$BalanceAmount;
-		  			@endphp
 		  			<tr>
-		  				<td>Concession</td>
-		  				<td>
-		  					<input type="hidden" name="concession" value="{{ $concession_amount }}">{{ $concession_amount }}
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>Previous Balance </td>
-		  				<td>{{ $BalanceAmount }}
-		  					<input type="hidden" name="previous_balance" value="{{ $BalanceAmount }}">
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>
-		  					Fine Scheme
-		  				</td>
-		  				<td>
-		  					<select name="fine_amount" id="fine_amount" onchange="($('#net_amount').val(parseInt({{ $net_amount }})+parseInt(this.value)),$('#deposit_amount').val(parseInt({{ $net_amount }})+parseInt(this.value)))">
-		  						<option disabled selected>Select </option> 
-		  						@foreach ($FineSchemes as $FineScheme)
-		  							<option value="{{ $FineScheme->fine_amount1 }}">Days After {{ $FineScheme->day_after1 }}</option> 
-		  						@endforeach 
-		  					</select> 
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>Net Amount </td>
-		  				<td> 
-		  					<input type="text" name="net_amount" id="net_amount" value="{{ $net_amount }}" readonly>
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>
-		  					Amount Deposit 
-		  				</td>
-		  				<td>
-		  					 <input type="text" name="deposit_amount" id="deposit_amount" onkeyup="$('#fee_balance').val({{ $net_amount }}-this.value)" value="{{ $net_amount }}" >
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>
-		  					Balance 
-		  				</td>
-		  				<td>
-		  					 <input type="text" name="fee_balance" value="0" id="fee_balance" readonly="">
-		  				</td>
-		  			</tr>
-		  			<tr>
-		  				<td>
-		  					Payment Mode 
-		  				</td>
-		  				<td>
-		  					<select name="payment_mode" id="payment_mode" onchange="paymentmode($('#payment_mode').val())">
-		  						@foreach ($paymentModes as $mode)
-		  							<option value="{{ $mode->name }}">{{ $mode->name }}</option> 
-		  						@endforeach 
-		  					</select> 
-		  				</td>
-		  			</tr>
-		  			<tr id="payment_mode_detail" style="display: none" colspan="2">
-		  				 
-		  				<td colspan="2">
-		  					<input type="text" name="refrence_no" placeholder="Refrence No">
-		  					<input type="text" name="payment_mode_date" placeholder="Date">
-		  					<input type="text" name="bank_name" placeholder="Bank Name">
-		  				</td>
-		  				 
+		  				<td>Net Amount</td>
+		  				<td>{{ $net_amount - $concession_amount }}  </td>
 		  			</tr>
 		  		</tbody>
 		  	</table>
+		  	<input type="hidden" name="net_amount" id="net_amount" value="{{ $net_amount - $concession_amount }}">
  		</div>
  		<div class="col-lg-6"> 
  			<input type="checkbox" class="checked_all" name="sibling" id="siblig_chk" onclick="showHide()">
@@ -119,9 +55,10 @@
 		 	 		<thead>
 		 	 			<tr>
 		 	 				<th>#</th>
-		 	 				<th>Sr</th>
-		 	 				<th>Registration No</th> 
-		 	 				<th>Name</th> 
+		 	 				<th>S.No</th>
+		 	 				 
+		 	 				<th>Class</th> 
+		 	 				<th>Registration No</th>
 		 	 				<th>Amount</th> 
 		 	 			</tr>
 		 	 		</thead>
@@ -130,45 +67,130 @@
 		 	 			$sr=1; 
 		 	 			$sib =0;
 		 	 			@endphp
-		 	 			<input type="hidden" name="month" value="{{ $request->month }}">
-		 	 			<input type="hidden" name="year" value="{{ $request->year }}">
-		 	 			@foreach ($siblings as $sibling) 
-		 	 			@php
-	 	 				$StudentFeeDetail = new App\model\StudentFeeDetail(); 
-	 	 				$studentSiblingFee = $StudentFeeDetail->getFeeDetailsByUpTodate($toDate,$sibling->studentSiblings->id); 
-		 	 			@endphp 
-		 	 			<tr> 
-		 	 				<td><input type="checkbox"  class="checkbox" name="student_id[]" value="{{ $sibling->studentSiblings->id }}"  > </td>
+		 	 			
+		 	 			@foreach ($siblings as $sibling)  
+		 	 			@if ($sibling->registration_no!=null)
+		 	 				<tr> 
+		 	 				<td><input type="checkbox"  class="checkbox" name="student_id[]" value="{{ $sibling->id }}"  > </td>
 		 	 				<td> {{ $sr++ }} </td>
-		 	 				<td>{{ $sibling->studentSiblings->registration_no or '' }}</td>
-		 	 				<td>{{ $sibling->studentSiblings->name  or ''}}</td>
-		 	 				<td><input type="hidden" name="" value="{{ $sib +=$studentSiblingFee->sum('fee_amount')-$studentSiblingFee->sum('concession_amount') }}">
-		 	 					{{ $studentSiblingFee->sum('fee_amount')-$studentSiblingFee->sum('concession_amount') }}
-		 	 				</td> 
-		 	 			@endforeach	
-		 	 			</tr>
+		 	 				<td> {{ $sibling->name }} </td>
+		 	 				<td>{{ $sibling->registration_no }}</td>
+		 	 				<td>{{ $sibling->total_dues}}</td>
+		 	 				 
+		 	 				
+		 	 			</tr> 
+		 	 			@endif
+		 	 			
+		 	 			@endforeach
 		 	 		</tbody>
 		 	 	</table>
-		 	 	<h4>Grand Total : {{ $net_amount + $sib }}</h4>
+		 	 	{{-- <h4>Grand Total : {{ $net_amount + $sib }}</h4> --}}
 			 </div> 
  		</div> 
+ 		<div class="col-md-12">
+ 			<table class="table table-striped" style="font-size: 100%;margin-bottom:0px;">
+ 			
+ 			   <tr id="id1" class="tr_clone">
+ 			       <td style="width: 200px;">
+ 			         <label class="control-label mb-2 text-left">Payment Mode <span style="color:red;">*</span></label> 
+ 			           <select name="payment_mode" class="form-control" id="payment_mode">
+		  						@foreach ($paymentModes as $mode)
+		  							<option value="{{ $mode->name }}">{{ $mode->name }}</option> 
+		  						@endforeach 
+		  					</select>  
+ 			       </td>
+ 			       <td style="width: 200px;">
+ 			         <label class="control-label mb-2 text-left">Amount <span style="color:red;">*</span></label> 
+ 			           <input type="text"  name="amount[]" onkeyup="feeSum()" value="{{ $net_amount }}" class="form-control fee_sum">    
+ 			       </td style="width: 250px;">
+ 			       <td><label class="control-label mb-2 text-left">Transaction / Cheque No <span style="color:red;">*</span></label> 
+ 			       	<input type="text"  name="cheeque_no[]" class="form-control"></td>
+ 			       <td><label class="control-label mb-2 text-left">Bank Name <span style="color:red;">*</span></label> 
+ 			           <input type="text"  name="bank_name[]" class="form-control"></td>
+ 			       <td><label class="control-label mb-2 text-left">Remarks <span style="color:red;">*</span></label> 
+ 			           <textarea type="text" name="remarks[]" class="form-control"></textarea></td>
+ 			       <td> <br><br> 
+ 			        <img src="{!! asset('img/details_open.png') !!}" class="tr_clone_add">   
+ 			        <img src="{!! asset('img/details_close.png') !!}" onclick="cloneRemove(this)">   
+ 			    </td> 
+ 			   </tr>
+ 			   <tr>
+ 			   	<td>Amount Deposit</td>
+ 			   	<td>		   		
+ 			   		<input type="text" id="amount_deposit" readonly  name="amount_deposit"  class="form-control">
+ 			   	</td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   </tr>
+ 			   <tr>
+ 			   	<td>Balance</td>
+ 			   	<td>		   		
+ 			   		<input type="text" id="fee_balance" readonly  name="fee_balance" value="0"  class="form-control">
+ 			   	</td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   	<td></td>
+ 			   </tr>
+ 			</table>
+ 		</div>
   </div>
   <div class="panel-footer text-center">
   	 @if ($paid==0)
-  	 	 <button type="button" id="feeCollectionSubmit_btn" class="btn btn-success" onclick="feeCollectionSubmit()">Submit</button> 
-   	{{-- <button type="button" class="btn btn-success" onclick="feeCollectionPrint()">Print</button>  --}}
-   	  {{-- <label style="margin-left: 10px;" class="btn btn-default"> --}}
-   	    <input type="checkbox" name="print" checked autocomplete="off" style="margin-left: 20px"> Print
-
-   	  </label>
+  	 	 <button type="button" id="feeCollectionSubmit_btn" class="btn btn-success" onclick="feeCollectionSubmit()">Submit</button>    
+   	    <input type="checkbox" name="print" checked autocomplete="off" style="margin-left: 20px"> Print 
    	  @else
    	   <button type="button" disabled="" class="btn btn-success">Paid</button>
-  	 @endif
-   	
-   	  
+  	 @endif 
   </div>
 </div>
  	 
 </form>
  
- 
+ <script>
+ feeSum();
+ function feeSum() { 
+ 	var fee_sum = parseFloat(0);
+    $(".fee_sum").each(function(){
+        fee_sum += +$(this).val();
+    });    
+    var net_amount = parseFloat($('#net_amount').val())
+   var amount_deposit= parseFloat($("#amount_deposit").val(fee_sum));
+   var fee_balance= parseFloat($("#fee_balance").val(net_amount - fee_sum));        
+           
+ }
+ // function addRow(){
+ var regex = /^(.*)(\d)+$/i;
+ var cindex = 1;
+
+ $(".tr_clone_add").on('click', function() {
+ 	if (cindex <=1) {
+ 		var $tr    = $(this).closest('.tr_clone');
+ 		var $clone = $tr.clone(true);
+ 		cindex++;
+ 		$clone.find(':text').val('');
+ 		$clone.attr('id', 'id'+(cindex) ); //update row id if required
+ 		//update ids of elements in row
+ 		$clone.find("*").each(function() {
+ 		        var id = this.id || "";
+ 		        var match = id.match(regex) || [];
+ 		        if (match.length == 3) {
+ 		            this.id = match[1] + (cindex);
+ 		        }
+ 		});
+ 		$tr.after($clone); 
+ 	}
+ 	 
+    
+ });
+ function cloneRemove(obj){
+   var numTrclone = $('.tr_clone').length;
+   cindex--;
+   if(numTrclone>1){ 
+   $(obj).parents(".tr_clone").remove();
+   }
+ }
+
+ </script>
