076d3999d7874c5dbfe5d4893f59ae319106d1c1
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-06-04 23:24:51 +0530

    update notification

--- resources/views/admin/notification/list.blade.php
+++ resources/views/admin/notification/list.blade.php
@@ -9,9 +9,9 @@
 
 @endif
 <li style="{{ $style }}">
-  <a href="#">
+  <a href="{{ $notification->link }}">
      
-    <h4 style="margin-left:0px">
+    <h4 style="margin-left:0px" onclick="callAjax(this,'{{ route('notification.read.status',Crypt::encrypt($notification->id)) }}')">
       {{ $notification->message }}
       <small><i class="fa fa-clock-o"></i> {{ $notification->created_at->diffForHumans() }}</small>
     </h4>
