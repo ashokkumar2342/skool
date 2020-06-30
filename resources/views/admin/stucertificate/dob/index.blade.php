459d1495595cc404abf96adf3bb44f7f15666b3f
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-06-30 17:39:47 +0530

    Update index.blade.php

--- resources/views/admin/stucertificate/dob/index.blade.php
+++ resources/views/admin/stucertificate/dob/index.blade.php
@@ -6,7 +6,7 @@
     <section class="content" style="margin-top: -15px"> 
       <div class="box">
         <div class="box-body">
-        <form action="{{ route('admin.student.BirthCertificateApplicationStore') }}" method="post" class="add_form">
+        <form action="{{ route('admin.student.BirthCertificateApplicationStore') }}" method="post" class="add_form" button-click="dob_btn_show" no-reset="true">
         {{ csrf_field() }}
         <div class="row"> 
           <div class="col-lg-2 form-group">
@@ -15,7 +15,7 @@
           </div>
           @if (@$studentdetail->id==0)
           <div class="col-lg-1 form-group">
-            <button type="button" success-popup="true" class="btn btn-primary" style="margin-top: 24px" onclick="callAjax(this,'{{ route('admin.student.BirthCertificateApplicationForm') }}'+'?Registration_no='+$('#Registration_no').val(),'slc_form_div')">Show</button>
+            <button type="button" success-popup="true" id="dob_btn_show" class="btn btn-primary" style="margin-top: 24px" onclick="callAjax(this,'{{ route('admin.student.BirthCertificateApplicationForm') }}'+'?Registration_no='+$('#Registration_no').val(),'slc_form_div')">Show</button>
           </div> 
            @endif 
           <div id="slc_form_div">
