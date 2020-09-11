bd7fc974c7ce99b6b3a7f566f8649c464410c408
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-09-11 23:34:46 +0530

    Update AdminController.php

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -199,6 +199,26 @@ class AdminController extends Controller
             return $e;
         }
        
+    }   
+    public function homeworkStore(Request $request){ 
+        try {  
+            $homework = new Homework();
+            $homework->class_id = $request->class_id;
+            $homework->section_id = $request->section_id;
+            $homework->homework = $request->homework;
+            $homework->created_by = $request->user_id;
+            $homework->date = $request->date == null ? $request->date : date('Y-m-d',strtotime($request->date)); 
+             
+            $homework->save();
+            $response = array();
+            $response['status'] = 1;
+            $response['msg'] = "Homework Created Successfully";
+            return $response;
+             
+        } catch (Exception $e) {
+            return $e;
+        }
+       
     }
     public function homeworkToday(Request $request,$id){ 
         try {   
