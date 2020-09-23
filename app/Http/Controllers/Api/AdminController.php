f2809da3402c7d33fdc2bb6829dfddabad4f28d0
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-09-23 22:49:13 +0530

    Update AdminController.php

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -240,6 +240,7 @@ class AdminController extends Controller
     public function attendanceStore(Request $request){ 
         try {  
             $user_id=$request->user_id;
+            $date=$request->date;
             $rules=[ 
             'class_id' => 'required', 
             'section_id' => 'required', 
