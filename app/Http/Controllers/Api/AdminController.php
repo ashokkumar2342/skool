d3ec128ae3292df0880f7ba1f6a4ee394e02daf3
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-10-10 16:13:00 +0530

    Update AdminController.php

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -320,8 +320,9 @@ class AdminController extends Controller
     }
     public function classTestStatus($id){ 
         try {   
-            $classTestStatus=DB::select(DB::raw("call up_check_classtest_status ('$id')")); 
-            $classTestStatuss=implode(',', $classTestStatus);
+             $classTestStatus=DB::select(DB::raw("call up_check_classtest_status ('$id')")); 
+
+           $classTestStatuss=explode(',', $classTestStatus[0]->TestStatus); 
              if (isset($classTestStatuss)) {
                return response()->json(['data'=>$classTestStatuss,'status'=>'1','msg'=>'success']);   
              }
